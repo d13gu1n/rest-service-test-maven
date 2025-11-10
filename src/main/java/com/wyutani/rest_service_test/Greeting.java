4eeea8017331553b78c47d95d703df15be3ff152
dacevedom <d.acevedom1993@gmail.com>
2025-11-10 13:10:22 -0300

    First iteration on REST Service creation using Springboot + Maven

---
+++ src/main/java/com/wyutani/rest_service_test/Greeting.java
@@ -0,0 +1,5 @@
+package com.wyutani.rest_service_test;
+
+public record Greeting (long id, String content){ 
+    
+}
---
+++ src/main/java/com/wyutani/rest_service_test/GreetingController.java
@@ -0,0 +1,8 @@
+package com.wyutani.rest_service_test;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+// TODO: Continue with imports -> https://spring.io/guides/gs/rest-service
+public class GreetingController {
+    
+}
