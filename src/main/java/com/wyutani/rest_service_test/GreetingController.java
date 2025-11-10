74537eddc3e8f074c857fd40ccd47b4e54cff96f
dacevedom <d.acevedom1993@gmail.com>
2025-11-10 15:58:25 -0300

    Created REST Service creation test using Springboot + Maven

--- src/main/java/com/wyutani/rest_service_test/GreetingController.java
+++ src/main/java/com/wyutani/rest_service_test/GreetingController.java
@@ -2,7 +2,17 @@ package com.wyutani.rest_service_test;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-// TODO: Continue with imports -> https://spring.io/guides/gs/rest-service
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GreetingController {
-    
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
 }
